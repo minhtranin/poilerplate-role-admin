4579db820c0cf4dabf4bb9b7a192d4d5517cbdfa
MinhTran <minhtran.in@outlook.com>
2020-04-28 16:24:17 +0700

    add source

---
+++ .umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  locale: { antd: true },
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/menus' },
+  ],
+});
